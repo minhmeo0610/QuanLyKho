87f3b9cadd07fd34f49a65085e8e189103d6b684
NgocAnh <ngocanh411997@gmail.com>
2018-04-21 23:39:01 +0700

    [Ánh]

--- QuanLyKho/QuanLyKho/VIEW/frmNhanVien.Designer.cs
+++ QuanLyKho/QuanLyKho/VIEW/frmNhanVien.Designer.cs
@@ -517,6 +517,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(1107, 532);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox4);
