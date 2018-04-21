1fe0ae4889fcac8393335b98f8ea550a6c7a86ec
NgocAnh <ngocanh411997@gmail.com>
2018-04-21 21:51:45 +0700

    [Ánh] Thủ tục Nhân Viên

---
+++ ThuTuc[Ánh].sql
@@ -0,0 +1,45 @@
+﻿-- Sửa bảng Nhân Viên
+GO
+ALTER TABLE dbo.NhanVien 
+ALTER COLUMN GioiTinh NVARCHAR(5)
+-- Thủ tục Nhân Viên
+-- Xem Nhân Viên
+GO
+CREATE PROC XemNV
+AS
+BEGIN
+	SELECT *
+	FROM dbo.NhanVien
+END
+-- Thêm NV
+GO
+CREATE PROC ThemNV(@MaNV NCHAR(10), @TenNV NVARCHAR(50), @NgaySinh DATE,@GioiTinh NVARCHAR(5), @DiaChi NVARCHAR(50), @SoDT NCHAR(20))
+AS
+BEGIN
+	INSERT INTO dbo.NhanVien
+	        ( MaNV ,
+	          TenNV ,
+	          NgaySinh ,
+	          GioiTinh ,
+	          DiaChi ,
+	          SoDT
+	        )
+	VALUES  ( @MaNV,@TenNV,@NgaySinh,@GioiTinh,@DiaChi,@SoDT)
+END
+-- Sửa NV
+GO
+CREATE PROC SuaNV(@MaNV NCHAR(10), @TenNV NVARCHAR(50), @NgaySinh DATE,@GioiTinh NVARCHAR(5), @DiaChi NVARCHAR(50), @SoDT NCHAR(20))
+AS
+BEGIN
+	UPDATE dbo.NhanVien
+	SET TenNV=@TenNV,NgaySinh=@NgaySinh,GioiTinh=@GioiTinh,DiaChi=@DiaChi,SoDT=@SoDT
+	WHERE MaNV=@MaNV
+END
+-- Xóa NV
+GO
+CREATE PROC XoaNV(@MaNV NCHAR(10))
+AS
+BEGIN
+	DELETE dbo.NhanVien
+	WHERE MaNV=@MaNV
+END
